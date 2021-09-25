1a9e4daaf6ada9ab1505eb3e8b095fc34a894a88
Wylujany <gejzowany@gmail.com>
2021-09-25 18:45:30 +0200

    Added class

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ bookButton.addEventListener("click", addBookToLibrary);
 
 let myLibrary = JSON.parse(localStorage.getItem("library")) || [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function addBookToLibrary() {
